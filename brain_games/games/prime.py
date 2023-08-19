091a80431e33863b6d3da9fa00d4f491d3ed5a00
revanhol <jadiknightrevan@gmail.com>
2023-08-19 23:47:01 +0300

    step 8

---
+++ brain_games/games/prime.py
@@ -0,0 +1,20 @@
+from random import randint
+
+game_rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def create_question():
+    number = randint(2, 100)
+    count = 0
+    i = 2
+    while i < number//2:
+        if number % i == 0:
+            count += 1
+            i += 1
+        else:
+            i += 1
+    if count == 0:
+        answer = 'yes'
+    else:
+        answer = 'no'
+    return str(number), answer
