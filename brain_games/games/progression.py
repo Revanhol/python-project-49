5da0825aff2ee42621d78b4166747b57d1d8db35
revanhol <jadiknightrevan@gmail.com>
2023-08-20 23:47:29 +0300

    update

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -6,7 +6,7 @@ game_rules = 'What number is missing in the progression?'
 def create_question():
     first_number = random.randint(1, 10)
     last_number = random.randint(11, 100)
-    step_number = random.randint(1, 5)
+    step_number = random.randint(2, 5)
     progression = list(range(first_number, last_number, step_number))
     change_index = random.randint(0, len(progression) - 1)
     answer = str(progression[change_index])
